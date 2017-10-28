d313e7c9fbab1902b93686754e08d88569223630
futstone <hairetdin.nico@gmail.com>
2017-10-29 00:27:15 +0300

    styling

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -11,7 +11,7 @@ export class HomePage {
   username: string;
 
   constructor(private fire: AngularFireAuth, public navCtrl: NavController, public navParams: NavParams) {
-    //this.username = this.fire.auth.currentUser.email;
+    this.username = this.fire.auth.currentUser.email;
   }
 
   ionViewDidLoad() {
