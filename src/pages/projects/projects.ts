1dea65778f9df31c7f92a0d4520e77849e02ae18
futstone <hairetdin.nico@gmail.com>
2017-10-28 12:54:55 +0300

    add content to projects-page

--- src/pages/projects/projects.ts
+++ src/pages/projects/projects.ts
@@ -1,19 +1,13 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the ProjectsPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
 @IonicPage()
 @Component({
   selector: 'page-projects',
   templateUrl: 'projects.html',
 })
 export class ProjectsPage {
+  project: string = "futoshiki";
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
